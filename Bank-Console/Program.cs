d2561c7c824d409438eba870e1a4358bf16e5374
Mateusz Drzyzga <mdrzyzdu@gmail.com>
2019-04-10 16:02:04 +0200

    AccountsManager

--- Bank-Console/Program.cs
+++ Bank-Console/Program.cs
@@ -10,47 +10,20 @@ namespace Bank_Console
     {
         static void Main(string[] args)
         {
-            string name = "Nazwa: Bank";
-            string author = "Autor: Mateusz Drzyzga";
-            Console.WriteLine(name);
-            Console.WriteLine(author);
-            Console.WriteLine();
+            AccountsManager manager = new AccountsManager();
 
-            List<Account> accounts = new List<Account>();
+            manager.CreateBillingAccount("Mateusz", "Drzyzga", 1234567890);
+            manager.CreateSavingsAccount("Mateusz", "Drzyzga", 1234567890);
+            manager.CreateSavingsAccount("Aaaaa", "Bbbbb", 0987654321);
 
-            accounts.Add(new SavingsAccount(1, "Marek", "Zajac", 1234567890));
-            accounts.Add(new SavingsAccount(2, "Marek", "Aaaa", 1234554321));
-            accounts.Add(new SavingsAccount(3, "Zzzz", "Bbb", 0987654321));
-            accounts.Add(new BillingAccount(4, "Marek", "Zajac", 1234567890));
-            accounts.Add(new BillingAccount(5, "Marek", "Aaaa", 1234554321));
+            IList<Account> accounts = (IList<Account>)manager.GetAllAccounts();
 
             Printer printer = new Printer();
 
-            printer.Print(accounts[1]);
-            printer.Print(accounts[4]);
+            printer.Print(accounts[0]);
+            printer.Print(accounts[2]);
 
-            Console.ReadKey();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            Console.ReadKey();
+            Console.ReadKey();            
         }
     }
 }
